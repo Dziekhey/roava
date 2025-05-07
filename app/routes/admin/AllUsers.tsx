beea3aba9be70cef695f3e0c42d55480427a5365
Dziekhey <dakeysandra@gmail.com>
2025-05-07 19:54:36 +0000

    dashboard layout done

--- app/routes/admin/AllUsers.tsx
+++ app/routes/admin/AllUsers.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Header } from "../../../components";
+import React from "react";
 
 const AllUsers = () => {
   return (
-    <div>AllUsers</div>
-  )
-}
+    <main className="dashboard wrapper">
+      <Header
+        title="Trips Page"
+        description="Check out our current users in real time"
+      />
+      All Users Page Contents
+    </main>
+  );
+};
 
 export default AllUsers;
--- app/routes/admin/Dashboard.tsx
+++ app/routes/admin/Dashboard.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Header } from "../../../components";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const user = {
+    name: "Seraphina",
+    email: "sera@gmail.com",
+    imageUrl: "/assets/images/david.webp",
+  };
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user?.name ?? "Guest"} 👏`}
+        description="Track activity, trends and popular destinations in real time"
+      />
+      Dashboard Page Contents
+    </main>
+  );
 };
 
 export default Dashboard;
